c2bd74f722704c5207bd0b5761be0324a6631129
ngoclong <ngoclong.thaihoa@gmail.com>
2023-10-23 11:46:04 +0700

    ha version idk-15

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,32 @@
 package org.example;
+
+
+import com.spire.xls.FileFormat;
 import com.spire.xls.Workbook;
 import com.spire.xls.Worksheet;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
 //C:/Users/Admin/Downloads/input.xlsx
 public class Main {
-    public static void main(String[] args){
-//Create a Workbook instance and load an Excel file
+    public static void main(String[] args) throws IOException {
+
+
+        //Create a Workbook instance and load an Excel file
         Workbook workbook = new Workbook();
         workbook.loadFromFile("input.xlsx");
 
-        //Set worksheets to fit to width when converting
-        workbook.getConverterSetting().setSheetFitToWidth(true);
+        //Set worksheets to fit to page when converting
+        workbook.getConverterSetting().setSheetFitToPage(true);
+        workbook.getConverterSetting().setYDpi(500);
+        //Save the resulting document to a specified path
+        workbook.saveToFile("output/test.pdf", FileFormat.PDF);
 
-        //Get the first worksheet
-        Worksheet worksheet = workbook.getWorksheets().get(0);
-
-        //Convert to PDF and save the resulting document to a specified path
-        worksheet.saveToPdf("output/WorksheetToPdf.pdf");
     }
+
+
 
 }
