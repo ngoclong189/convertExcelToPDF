e034abf158fbc9673cb5f66cd7b34e905dc6b39d
ngoclong <ngoclong.thaihoa@gmail.com>
2023-10-25 15:36:41 +0700

    dm lib

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,18 @@
 package org.example;
 
+import com.groupdocs.conversion.Converter;
+import com.groupdocs.conversion.options.convert.PdfConvertOptions;
 
-import com.spire.xls.FileFormat;
-import com.spire.xls.Workbook;
-import com.spire.xls.Worksheet;
-
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-
-
 //C:/Users/Admin/Downloads/input.xlsx
 public class Main {
     public static void main(String[] args) throws IOException {
+        Converter converter = new Converter("input.xlsx");
+        PdfConvertOptions options = new PdfConvertOptions();
 
-
-        //Create a Workbook instance and load an Excel file
-        Workbook workbook = new Workbook();
-        workbook.loadFromFile("input.xlsx");
-
-        //Set worksheets to fit to page when converting
-        workbook.getConverterSetting().setSheetFitToPage(true);
-        workbook.getConverterSetting().setYDpi(500);
-        //Save the resulting document to a specified path
-        workbook.saveToFile("output/test.pdf", FileFormat.PDF);
+        converter.convert("converted.pdf", options);
 
     }
-
-
 
 }
